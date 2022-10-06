6e4c0ee08c803e1fb1466343d8c18814cb2c6215
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-10-06 18:29:00 -0300

    Fix: Project

--- src/main/java/com/devsuperior/bds03/dto/DepartmentDTO.java
+++ src/main/java/com/devsuperior/bds03/dto/DepartmentDTO.java
@@ -1,41 +1,41 @@
 package com.devsuperior.bds03.dto;
 
-import java.io.Serializable;
-
 import com.devsuperior.bds03.entities.Department;
 
+import java.io.Serializable;
+
 public class DepartmentDTO implements Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	public Long id;
-	public String name;
-	
-	public DepartmentDTO() {
-	}
+    private static final long serialVersionUID = 1L;
 
-	public DepartmentDTO(Long id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-	
-	public DepartmentDTO(Department entity) {
-		id = entity.getId();
-		name = entity.getName();
-	}
+    public Long id;
+    public String name;
 
-	public Long getId() {
-		return id;
-	}
+    public DepartmentDTO() {
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public DepartmentDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public DepartmentDTO(Department entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
