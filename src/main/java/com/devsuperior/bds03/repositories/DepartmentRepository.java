11151539e697ee9034da213a78cab135db3460c4
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-10-07 10:03:50 -0300

    Query methods

--- src/main/java/com/devsuperior/bds03/repositories/DepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.devsuperior.bds03.repositories;
-
-import com.devsuperior.bds03.entities.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-
-}
